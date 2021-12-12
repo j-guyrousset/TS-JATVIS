ee612d20e98aeb198ea7934a6d16de66c24fe167
Jean-Guy Rousset <rousset.jean-guy@laposte.net>
2021-12-12 14:24:31 +0100

    add speech engine

--- main.py
+++ main.py
@@ -6,3 +6,14 @@ BOTNAME = config('BOTNAME')
 
 
 engine = pyttsx3.init('sapi5')
+
+#set rate
+engine.setProperty('rate', 190)
+
+#set volume
+engine.setProperty('volume', 1.0)
+
+#set voice
+voices = engine.getProperty('voices')
+#engine.setProperty('voice', voices[0]) #male voice
+engine.setProperty('voice', voices[1]) #female voice
