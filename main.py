fd7a650753e1a70d860d28375d2000a8a558c435
Jean-Guy Rousset <rousset.jean-guy@laposte.net>
2021-12-12 14:37:53 +0100

    update speech engine

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ BOTNAME = config('BOTNAME')
 engine = pyttsx3.init('sapi5')
 
 #set rate
-engine.setProperty('rate', 190)
+engine.setProperty('rate', 200)
 
 #set volume
 engine.setProperty('volume', 1.0)
@@ -16,4 +16,11 @@ engine.setProperty('volume', 1.0)
 #set voice
 voices = engine.getProperty('voices')
 #engine.setProperty('voice', voices[0]) #male voice
-engine.setProperty('voice', voices[1]) #female voice
+engine.setProperty('voice', voices[2].id) #1:female voice - 2:male voice - 0: default
+
+
+
+engine.say("You talking to me?")
+engine.say("who do you think I am?")
+engine.runAndWait()
+
